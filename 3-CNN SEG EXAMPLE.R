fce39d2288446362eeb554241d6d803e5070e259
Allan <allan.sauvat@gmail.com>
2022-05-20 17:21:39 +0200

    Code optimized

--- 3-CNN SEG EXAMPLE.R
+++ 3-CNN SEG EXAMPLE.R
@@ -1,3 +1,7 @@
+library(tensorflow)
+library(keras)
+tf$print('Tensorflow initialized')
+###
 library(EBImage)
 library(RBioFormats)
 library(MetaxpR)
@@ -6,19 +10,6 @@ library(MorphR)
 library(doParallel)
 library(pbapply)
 library(magrittr)
-###
-library(tensorflow)
-library(keras)
-library(unet)
-#=================================================================================================================================================================#
-#CONFIGURE GPU
-#=================================================================================================================================================================#
-config = tf$compat$v1$ConfigProto(gpu_options = tf$compat$v1$GPUOptions(allow_growth=TRUE),
-                                  allow_soft_placement=TRUE,
-                                  log_device_placement=FALSE,
-                                  device_count = dict('GPU', 1))
-
-sess = tf$compat$v1$Session(config=config)
 
 #=================================================================================================================================================================#
 #GATHER ALL PLATE INFORMATION
@@ -26,133 +17,110 @@ sess = tf$compat$v1$Session(config=config)
 SERVER = 'NEO-SERVER'
 DB = GetMDCInfo(SERVER)
 #-----
-PID = c(911);names(PID)=PID 
-PLT = DB[sapply(PID,function(p)tail(which(DB$PlateID==p),1)),]
-PIF = lapply(PID,function(p)GetPINFO(SERVER,PlateID=p))
-alls = lapply(PIF,function(x)lapply(1:nrow(x$SID),function(i)x$SID[i,]))
+PID = 21328;names(PID)=PID 
+PLT = DB[tail(which(DB$PlateID==PID),1),]
+PIF = GetPINFO(SERVER,PlateID=PID)
 #-----
-INF = lapply(PID,function(p)lapply(GetIMPath(SERVER,PlateID=p,WellID=PIF[[as.character(p)]]$WID[1],SiteID=alls[[as.character(p)]][[1]]),function(x)read.metadata(x)))
-cn = lapply(INF,function(x){ci=sapply(x,function(y)y$globalMetadata$'image-name')});print(cn)
-cn = apply(do.call('cbind',cn),1,unique);print(cn) #If all cn are the same
-names(cn)=cn
-Di = min(sapply(INF,function(x)x[[1]]$coreMetadata$sizeX))
+INF = lapply(GetIMPath(SERVER,PlateID=PID,WellID=PIF$WID[1],SiteID=PIF$SID[1,]),function(x)read.metadata(x)) #Read metadata
+cn = sapply(INF,function(x)x$globalMetadata$'image-name');names(cn)=cn #Channel acquisition names
+Di = INF[[1]]$coreMetadata$sizeX #Image dimension
 #----
-PM.loc = '/media/Hcs-screen10-vi/D/Platemap/'
-PM = lapply(PID,function(p)subset(read.csv(paste0(PM.loc,SERVER,'/',p,'.csv'),sep=',',stringsAsFactors = F),Drug!=''))
+PM.loc = '/media/Hcs-screen10-vi/TEAM_DATA/Platemap/' #Platemap location
+PM = subset(read.table(paste0(PM.loc,SERVER,'/',PID,'.csv'),sep=',',stringsAsFactors = F,header=T,encoding='latin1'),Drug!='') #Plate layout
 #-----
-rgp = lapply(PID,function(p){
-  woi = intersect(subset(PM[[as.character(p)]],Drug=='DMSO')$WellHeader,PIF[[as.character(p)]]$WID) 
-  if(length(woi)>5){woi = woi[sample(1:length(woi),5,replace=T)]}
-  IM = lapply(seq_along(cn),function(i)readImage(sapply(woi,function(w)GetIMPath(SERVER,WellID=w,SiteID=alls[[as.character(p)]][[1]],PlateID=p)[i])))
-  rg = lapply(IM,function(x){qu=quantile(x,probs=seq(0,1,5*10**-4));return(qu[c(2,length(qu)-1)])});names(rg)=cn
-  return(rg)
-})
+nCores=32;nF=3
+id = c(seq(1,length(PIF$WID),by=nCores*nF),length(PIF$WID)+1)
+woi = lapply(1:(length(id)-1),function(i){sort(PIF$WID)[id[i]:(id[i+1]-1)]}) #split well in batches
+rm(id)
 #----
-nCores=16
-woi = lapply(PID,function(p){
-  id = c(seq(1,length(PIF[[as.character(p)]]$WID),by=16),length(PIF[[as.character(p)]]$WID)+1)
-  lapply(1:(length(id)-1),function(i){sort(PIF[[as.character(p)]]$WID)[id[i]:(id[i+1]-1)]})
-}) #Forced to split Wells in "Well sets" not to saturate RAM
-#----
-sF = 128 # Size of image units
-save.image('PDATA.RData')
+sF = 512L # Size of image after binning
+save.image('PDATA.RData') #keep data stored
+
 #=================================================================================================================================================================#
 #LOAD MODEL
 #=================================================================================================================================================================#
-
-source('/media/Hcs-screen10-vi/Q/Allan/DeepLearning/CNN-Segmentation/NUCYTO-128/DL-TRAIN/dice_metrics.R')
-model = load_model_hdf5('/media/Hcs-screen10-vi/Q/Allan/DeepLearning/CNN-Segmentation/NUCYTO-128/DL-TRAIN/NuCytoFromTL.h5',c("dice" = dice))
+tr.dir = '/media/Hcs-screen10-vi/T/Allan/DEEPLEARNING/HDF5/' #Trained models location
+#
+source(paste0(tr.dir,'FUNCS/dice_metrics.R')) 
+modsem = unet::unet(input_shape = c(sF,sF,1),num_classes = 2,filters=32,num_layers = 3)%>%load_model_weights_hdf5(paste0(tr.dir,'tl512d-ncm.h5')) #model import
 #-------------------#
-nt = (Di/sF)**2 #Number of tiles composing each image
-dir.create('SEGS');sapply(PID,function(p){dir.create(paste0('SEGS/',p))})
+dir.create('SEGS');sapply(1:(PIF$TID[2]),function(ti){dir.create(paste0('SEGS/T',ti))}) #Folder where masks are going to be exported
 
 #=================================================================================================================================================================#
 #PROCESS
 #=================================================================================================================================================================#
-for(p in PID){
+for(ti in seq_len(PIF$TID[2])){ #Loop over time
   ct=1
-  for(wset in woi[[as.character(p)]]){
-    #IMAGES BATCH SPLIT#
-    ####################
+  for(wset in woi){ #Loop over well batches
+    
+    #IMAGES IMPORT & BINNING#
+    ########################
     cl = makeCluster(nCores)
     clusterExport(cl,setdiff(ls(),'DB'))
     invisible({clusterEvalQ(cl,{library(MetaxpR);library(EBImage);library(magrittr);library(MorphR);library(S4Vectors)})})
-    print(paste0('Splitting images, Plate #',p,' well batch ',ct,'/',length(woi[[as.character(p)]]),'...'))
-    pdat = pblapply(wset,function(w){
-      wdat = lapply(alls[[as.character(p)]],function(sxy){
-        #Read images ------------------------------ ----------------------------
-        IP = GetIMPath(SERVER,PlateID=p,WellID=w,SiteID=sxy);names(IP)=cn
-        IM = readImage(IP['TL 50'])
-        
-        #Adjust histograms-----------------------------------------------------
-        IN = normalize(IM,inputRange=rgp[[as.character(p)]][['TL 50']])
-        
-        #untile---------------------------------------------------------------
-        TL = untile(IN,nim=rep(Di/sF,2),lwd=0) #Splited image in blocks of size 128
-        return(TL)
-      })
-      wdat = do.call('abind',list(wdat,along=3))
+    print(paste0('Importing & preprocessing images, TimePoint#',ti,' well batch ',ct,'/',length(woi),'...'))
+    mw = pblapply(wset,function(w){ #Loop over well, multithread
+      ms = apply(PIF$SID,1,function(sxy){ #Loop over sites
+        IP = GetIMPath(SERVER,PlateID=PID,WellID=w,SiteID=sxy,TimePoint=ti) #Get image path
+        IM = readImage(IP)%>%autoNormalize(step=5*10**-4)%>%resize(w=sF) #Read, normalize, and bin image
+        return(IM)
+      },simplify=F)
+      ms = do.call('abind',list(ms,along=0))
       gc()
-      return(wdat)
+      return(ms)
     },cl=cl)
     stopCluster(cl);rm(cl)
-    pdat = do.call('abind',list(pdat,along=3))
-    pdat = do.call(abind,list(lapply(1:dim(pdat)[3],function(i)pdat[,,i]),along=0))
-    
-    #PREDICTION#
-    ############
-    print(paste0('Predicting masks from images, Plate #',p,' well batch ',ct,'/',length(woi[[as.character(p)]]),'...'))
-    pdat = predict(model,do.call('abind',list(pdat,along=4)))#Create OverallMask
+    names(mw)=wset
     gc()
     
-    #RECONSTITUTION#
-    ################
-    print(paste0('Reconstituting masks, Plate #',p,' well batch ',ct,'/',length(woi[[as.character(p)]]),'...'))
-    Ii = c(seq(1,nrow(pdat),by=nt),nrow(pdat)) #Image indexes in matrix
-    pdat = lapply(1:(length(Ii)-1),function(id){
-      N=tile(do.call('abind',list(lapply(Ii[id]:(Ii[id+1]-1),function(i){pdat[i,,,1]}),along=3)),nx=Di/sF,lwd=0)
-      C=tile(do.call('abind',list(lapply(Ii[id]:(Ii[id+1]-1),function(i){pdat[i,,,2]}),along=3)),nx=Di/sF,lwd=0)
-      return(list(N=N,C=C))
-    });gc()
-    #Labels
-    labs = unlist(lapply(wset,function(w){
-      sapply(alls[[as.character(p)]],function(sxy){
-        nm=paste0(w,'_sx',sxy[1],'_sy',sxy[2])
-        return(nm)
-      })
-    }))
-    names(pdat)=labs
+    #GPU-based SEMANTIC SEGMENTATION#
+    #################################
+    print(paste0('Predicting masks from images, TimePoint#',ti,' well batch ',ct,'/',length(woi),'...'))
+    mw = pblapply(names(mw),function(w){pd = predict(modsem,mw[[w]],batch_size = 8);rownames(pd)=paste(w,1:nrow(pd),sep='/');return(pd)})
+    names(mw) = wset
+    gc()
     
-    #ADDITIONNAL PROCESSING
-    #######################
-    print(paste0('Post-processing masks, Plate #',p,' well batch ',ct,'/',length(woi[[as.character(p)]]),'...'))
-    #Process and separate nuclei
+    #PROCESSING RAW PROBABILITY MASKS#
+    ##################################
+    print(paste0('Post-processing masks, Timepoint#',ti,' well batch ',ct,'/',length(woi),'...'))
     cl=makeCluster(nCores)
-    pdat = pblapply(pdat,function(IMi){
-      library(EBImage);library(MorphR);library(magrittr)
-      NM = IMi$N%>%medianFilter(size=5)%>%sigmoNormalize(scaled=T,z0=0.75,lambda=10**3);NM=NM>otsu(NM)
-      NM = propagate(NM,opening(NM,makeBrush(21,'disc')),NM)
-      NM = watershed(distmap(NM),tolerance=3)
-      #
-      CM = pmax(IMi$N,IMi$C)%>%medianFilter(size=5)%>%sigmoNormalize(scaled=T,z0=0.7,lambda=10**3)
-      CM = CM>otsu(CM);CM = closing(CM,makeBrush(5,'disc'))
-      CM = propagate(CM,opening(CM,makeBrush(41,'disc')),CM)
-      CM = propagate(CM,NM,CM)
-      return(list(NM=NM,CM=CM))
+    clusterExport(cl,'Di')
+    mw = pblapply(mw,function(tsr){
+      apply(tsr,1,function(IMi){
+        library(EBImage);library(MorphR);library(magrittr)
+        #
+        NM = IMi[,,1]%>%whiteTopHat(makeBrush(21,'disc',step=F))%>%thresh(w=10,h=10,offset=0.25)
+        NM = propagate(NM,opening(NM,makeBrush(7,'disc')),NM)
+        NM = watershed(distmap(NM),tolerance=1)
+        #
+        CM = (pmax(IMi[,,1],IMi[,,2])%>%sigmoNormalize(z0=5*10**-2,lambda=10**3))>0.95
+        CM = propagate(CM,opening(CM,makeBrush(11,'disc')),CM)
+        CM = propagate(IMi[,,2],NM,CM)-NM
+        #
+        MK = abind(NM,CM,along=3)%>%resize(w=Di,filter='none',antialias=FALSE)
+        return(MK)
+      },simplify = FALSE)
     },cl=cl)
     stopCluster(cl);rm(cl)
     gc()
     
-    #EXPORT#
-    ########
-    print(paste0('Exporting masks, Plate #',p,' well batch ',ct,'/',length(woi[[as.character(p)]]),'...'))
-    pbsapply(names(pdat),function(nm){
-      SaveSeg(pdat[[nm]]$N,ExportDir=paste0('SEGS/',p),FileName=paste0('N_',nm))
-      SaveSeg(pdat[[nm]]$C,ExportDir=paste0('SEGS/',p),FileName=paste0('C_',nm))
-    })
-    rm(pdat);gc()
+    #EXPORT REFINED MASKS#
+    #####################
+    print(paste0('Exporting Masks, Time#',ti,' well batch ',ct,'/',length(woi),'...'))
+    cl=makeCluster(nCores)
+    clusterExport(cl,c('SERVER','cn','PID','PIF','ti'))
+    invisible({clusterEvalQ(cl,{library(MetaxpR);library(EBImage);library(magrittr);library(MorphR);library(S4Vectors)})})
+    pbsapply(mw,function(tsr){
+      w = strsplit(names(tsr),'/')[[1]][1]
+      sapply(seq_along(tsr),function(i){
+        SaveSeg(tsr[[i]][,,1],ExportDir=paste0('SEGS/T',ti),FileName=paste0('N_',w,'_sx',PIF$SID[i,1],'_sy',PIF$SID[i,2]),asRDS=T)
+        SaveSeg(tsr[[i]][,,2],ExportDir=paste0('SEGS/T',ti),FileName=paste0('C_',w,'_sx',PIF$SID[i,1],'_sy',PIF$SID[i,2]),asRDS=T)
+      })
+    },cl=cl)
+    stopCluster(cl);rm(cl)
+    rm(mw);gc()
     ct=ct+1
   }
-};rm(p,ct);gc()
+};rm(ti,ct);gc()
 
 
